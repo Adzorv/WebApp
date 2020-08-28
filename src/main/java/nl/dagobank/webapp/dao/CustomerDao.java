a6570bf341b146e4676df15f52a81bb438205132
Manuel Oostveen <oostvem1@ad.hva.nl>
2020-08-28 09:42:04 +0200

    CustomerDao toegevoegd

---
+++ src/main/java/nl/dagobank/webapp/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package nl.dagobank.webapp.dao;
+
+import nl.dagobank.webapp.domain.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends CrudRepository<Customer, Integer> {
+
+}
--- src/main/java/nl/dagobank/webapp/service/DatabaseService.java
+++ src/main/java/nl/dagobank/webapp/service/DatabaseService.java
@@ -1,10 +1,12 @@
 package nl.dagobank.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseService {
 
+    @Autowired
     public DatabaseService() {
         super();
     }
