1b3d628cc337f36a1aa2d5473661a513f3190190
Manuel Oostveen <oostvem1@ad.hva.nl>
2020-08-28 09:26:32 +0200

    DatabaseService toegevoegd

---
+++ src/main/java/nl/dagobank/webapp/service/DatabaseService.java
@@ -0,0 +1,17 @@
+package nl.dagobank.webapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+
+    public DatabaseService() {
+        super();
+    }
+
+    public void fillDatabase() {
+        // Logica hier
+    }
+
+
+}
