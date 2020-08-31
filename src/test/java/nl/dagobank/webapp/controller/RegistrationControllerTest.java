b47848f2a423ee4b51b5334a533d46229472af51
Toppie <topl2@ad.hva.nl>
2020-08-31 15:39:44 +0200

    aanpaasing testunit

--- src/test/java/nl/dagobank/webapp/controller/RegistrationControllerTest.java
+++ src/test/java/nl/dagobank/webapp/controller/RegistrationControllerTest.java
@@ -10,10 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,12 +26,16 @@ class RegistrationControllerTest {
     @Autowired
     PasswordGenerator passwordGenerator;
 
+    Customer customer;
+    Customer customer2;
+    LocalDate date;
+
     public RegistrationControllerTest() {
     }
 
     @BeforeEach
     void setUp() {
-        /*Customer customer = new Customer();
+        customer = new Customer();
         customer.setFirstName("Jan");
         customer.setPrefix("van de");
         customer.setLastName("Jansen");
@@ -43,18 +45,39 @@ class RegistrationControllerTest {
         customer.setPostCode("1014AA");
         customer.setCity("Amsterdam");
         customer.setPhoneNumber("0612345678");
-        customer.setBirthDate(new LocalDate(2000, 11, 11));//FIXME: how to input date?
+        //date = new LocalDate(2000, 11, 11);
+        //customer.setBirthDate(date);//FIXME: how to input date?
         customer.setEmail("janj@gmail.com");
         customer.setBsn(135076569);
         String password = passwordGenerator.generate(10);
         customer.setPassword(password);
         String userName = usernameGenerator.createUsername(customer.getFirstName(), customer.getLastName());
-        customer.setUserName(userName);*/
+        customer.setUserName(userName);
+
+
+        customer2 = new Customer();
+        customer2.setFirstName("Joop");
+        customer2.setPrefix("van de");
+        customer2.setLastName("Veer");
+        customer2.setStreetName("Singel");
+        customer2.setHouseNumber(2);
+        customer2.setHouseNumberAnnex("bg");
+        customer2.setPostCode("1011AZ");
+        customer2.setCity("Amsterdam");
+        customer2.setPhoneNumber("0624345678");
+        //customer.setBirthDate(new LocalDate(2000,11,1));
+        customer2.setEmail("joop@gmail.com");
+        customer2.setBsn(135076569);//je kunt niet 2x registreren met hetzelfde bsn dus geeft het een foutmelding ??/**/
+        password = passwordGenerator.generate(10);
+        customer.setPassword(password);
+        userName = usernameGenerator.createUsername(customer.getFirstName(), customer.getLastName());
+        customer.setUserName(userName);
 
     }
 
     @AfterEach
     void tearDown() {
+        //customerDao.deleteAll();
     }
 
     @Test
@@ -63,49 +86,15 @@ class RegistrationControllerTest {
 
     @Test
     void testRegistrationHandler() {
-        Customer customer = new Customer();
-        customer.setFirstName("Jan");
-        customer.setPrefix("van de");
-        customer.setLastName("Jansen");
-        customer.setStreetName("Overtoom");
-        customer.setHouseNumber(23);
-        customer.setHouseNumberAnnex("2 hoog");
-        customer.setPostCode("1014AA");
-        customer.setCity("Amsterdam");
-        customer.setPhoneNumber("0612345678");
-        //git acustomer.setBirthDate(new LocalDate(2011,11,1));//FIXME: how to input date?
-        customer.setEmail("janj@gmail.com");
-        customer.setBsn(135076569);
-        String password = passwordGenerator.generate(10);
-        customer.setPassword(password);
-        String userName = usernameGenerator.createUsername(customer.getFirstName(), customer.getLastName());
-        customer.setUserName(userName);
-
-       customerDao.save(customer);
-
+        customerDao.save(customer);
         assertNotNull(customerDao.findById(customer.getId()));
         assertTrue(customerDao.findByBsn(135076569).getPostCode().equals("1014AA"));
         assertTrue(customerDao.existsByUserName("JanJan001"));
         List<Customer> customerList = Arrays.asList(customerDao.findAllByCity("Amsterdam"));
         assertTrue(customerList.size() == 1);
 
-        Customer customer2 = new Customer();
-        customer2.setFirstName("Jan");
-        customer2.setPrefix("van de");
-        customer2.setLastName("Jansen");
-        customer2.setStreetName("Overtoom");
-        customer2.setHouseNumber(23);
-        customer2.setHouseNumberAnnex("2 hoog");
-        customer2.setPostCode("1014AA");
-        customer2.setCity("Amsterdam");
-        customer2.setPhoneNumber("0612345678");
-        //customer.setBirthDate(new LocalDate(2000,11,1));
-        customer2.setEmail("janj@gmail.com");
-        customer2.setBsn(135076569);//je kunt niet 2x registreren met hetzelfde bsn
-
+        //customerDao.save(customer2); //FIXme: how te testen voor foutmelding?
         assertTrue(customerDao.findById(customer2.getId()).equals(Optional.empty()));
-
-        //customerDao.delete(customer);
 
 
     }
