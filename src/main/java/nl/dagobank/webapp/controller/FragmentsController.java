0231d4cc61ce308b1a7951056545da32ab9a440c
Toppie <topl2@ad.hva.nl>
2020-09-01 14:47:10 +0200

    navigatie for everypage with fragments

--- src/main/java/nl/dagobank/webapp/controller/FragmentsController.java
+++ src/main/java/nl/dagobank/webapp/controller/FragmentsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FragmentsController {
 
-    @GetMapping("framents")
+    @GetMapping("fragments")
     public String getHome() {
         return "fragments";
     }
