50c652fd39e6a5606478eecd441991f6aec8136a
Toppie <topl2@ad.hva.nl>
2020-09-01 16:05:23 +0200

    check if Iban is already in DB

--- src/main/java/nl/dagobank/webapp/service/IbanGenerator.java
+++ src/main/java/nl/dagobank/webapp/service/IbanGenerator.java
@@ -1,50 +1,58 @@
 package nl.dagobank.webapp.service;
 
+import nl.dagobank.webapp.dao.BankAccountDao;
 import nl.dagobank.webapp.dao.CustomerDao;
+import nl.dagobank.webapp.dao.PrivateAccountDao;
 import nl.dagobank.webapp.domain.BankAccount;
 import org.iban4j.CountryCode;
 import org.iban4j.Iban;
+import org.iban4j.IbanFormatException;
+import org.iban4j.UnsupportedCountryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import java.util.Random;
-
 @Service
 public class IbanGenerator {
-    String iban;
+    Iban iban;
+    String accountnumber;
 
-    /*@Autowired
-    public CustomerDao customerDao;*/
+    @Autowired
+    public PrivateAccountDao privateAccountDao;
 
     public IbanGenerator() {
     }
 
-    public Iban createIban(){
-        Iban iban = Iban.random(CountryCode.NL);
-        iban = Iban.random();
-        iban = new Iban.Builder()
-                .countryCode(CountryCode.NL)
-                .bankCode("DAGO")
-                .buildRandom();
+    public Iban createIban() {
+        while (!privateAccountDao.existsByIban(iban)) {
+            try {
+                Iban iban = Iban.random(CountryCode.NL);
+                iban = Iban.random();
+                iban = new Iban.Builder()
+                        .countryCode(CountryCode.NL)
+                        .bankCode("DAGO")
+                        .buildRandom();
+                return iban;
+            } catch (IbanFormatException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (UnsupportedCountryException e) {
+                e.printStackTrace();
+            }
+        }
         return iban;
     }
 
-    public String getIban() {
+    public Iban getIban() {
         return iban;
     }
 
-    public void setIban(String iban) {
+    public void setIban(Iban iban) {
         this.iban = iban;
     }
 
     @Override
     public String toString() {
-        return "IbanGenerator{" +
-                "iban='" + iban + '\'' +
-                '}';
+        return iban.toString();
     }
 }
