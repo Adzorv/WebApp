2b9a97c7c744dc0a3a30190bd2167a6c7a9bc038
Sebastian Holzapfel <sebastian.holzapfel@hva.nl>
2020-09-02 23:41:23 +0200

    voorstel overview gemaakt

---
+++ src/main/java/nl/dagobank/webapp/controller/AccountViewController.java
@@ -0,0 +1,36 @@
+package nl.dagobank.webapp.controller;
+
+import nl.dagobank.webapp.dao.BankAccountDao;
+import nl.dagobank.webapp.dao.PrivateAccountDao;
+import nl.dagobank.webapp.domain.BankAccount;
+import nl.dagobank.webapp.domain.PrivateAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class AccountViewController {
+
+    @Autowired
+    BankAccountDao bankAccountDao;
+
+    @GetMapping("/accountView{id}")
+    ModelAndView AccountViewHandler(@RequestParam("id") int id){
+        ArrayList<Integer> convertedId = new ArrayList<>();
+        convertedId.add(id);
+        Iterable<BankAccount> selectedAccount = bankAccountDao.findAllById(convertedId);
+        String accountName = selectedAccount.iterator().next().getAccountName();
+        System.out.println(accountName);
+        ModelAndView modelAndView = new ModelAndView("accountView");
+        modelAndView.addObject("bankAccountName", accountName);
+
+        return modelAndView;
+    }
+}
