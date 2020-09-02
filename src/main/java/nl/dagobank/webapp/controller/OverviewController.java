afd02b26d586f4a1b49624d83b0f40de24699707
Vincent van der Struis <vincent.van.der.struis@hva.nl>
2020-09-02 10:33:14 +0200

    bankrekening toont nu op overzicht

--- src/main/java/nl/dagobank/webapp/controller/OverviewController.java
+++ src/main/java/nl/dagobank/webapp/controller/OverviewController.java
@@ -1,9 +1,13 @@
 package nl.dagobank.webapp.controller;
 
 import nl.dagobank.webapp.backingbeans.TestForm;
+import nl.dagobank.webapp.dao.BankAccountDao;
+import nl.dagobank.webapp.domain.BankAccount;
 import nl.dagobank.webapp.domain.Customer;
+import nl.dagobank.webapp.domain.PrivateAccount;
 import nl.dagobank.webapp.domain.User;
 import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +16,25 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @SessionAttributes( "user" )
 public class OverviewController {
 
+    @Autowired
+    private BankAccountDao bankAccountDao;
+
+
     @GetMapping("overview")
     public String overview(Model model) {
         Customer user = (Customer)model.getAttribute( "user" );
+        List<BankAccount> bankAccountsOfUser = bankAccountDao.findAllByAccountHolder(user);
+        System.out.println(bankAccountsOfUser);
         System.out.println(user);
+        model.addAttribute("bankAccounts", bankAccountsOfUser);
         return "overview";
     }
+
+    // Lijst met bankaccounts
 }
--- src/main/java/nl/dagobank/webapp/dao/BankAccountDao.java
+++ src/main/java/nl/dagobank/webapp/dao/BankAccountDao.java
@@ -2,13 +2,19 @@ package nl.dagobank.webapp.dao;
 
 import nl.dagobank.webapp.domain.BankAccount;
 import nl.dagobank.webapp.domain.Customer;
+import nl.dagobank.webapp.domain.User;
 import org.iban4j.Iban;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BankAccountDao extends CrudRepository<BankAccount, Integer> {
     boolean existsByIban(Iban iban);
-    BankAccount findAllByAccountHolder(String accountHolder);
+    List<BankAccount> findAllByAccountHolder(User accountHolder);
+
+
+
 }
 
