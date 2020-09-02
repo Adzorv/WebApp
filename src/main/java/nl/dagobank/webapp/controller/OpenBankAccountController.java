f660f1cf713ec7a06031039fc3443acb8a47d300
Sebastian Holzapfel <sebastian.holzapfel@hva.nl>
2020-09-02 21:33:57 +0200

    verbetering implementatie bank account aanmaken

--- src/main/java/nl/dagobank/webapp/controller/OpenBankAccountController.java
+++ src/main/java/nl/dagobank/webapp/controller/OpenBankAccountController.java
@@ -1,28 +1,22 @@
 package nl.dagobank.webapp.controller;
 
 import nl.dagobank.webapp.backingbeans.BankAccountNameForm;
-import nl.dagobank.webapp.backingbeans.RegistrationForm;
 import nl.dagobank.webapp.dao.BankAccountDao;
 import nl.dagobank.webapp.domain.Customer;
-import nl.dagobank.webapp.domain.PrivateAccount;
-import nl.dagobank.webapp.service.IbanGenerator;
-import org.iban4j.Iban;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.math.BigDecimal;
 
 
 @Controller
 @SessionAttributes( "user" )
 public class OpenBankAccountController {
 
+@Autowired
+BankAccountDao bankAccountDao;
 
     @GetMapping("/openBankAccount")
     public ModelAndView openBankAccountHandler(Model model) {
@@ -30,11 +24,13 @@ public class OpenBankAccountController {
         Customer user = (Customer) model.getAttribute("user");
         modelAndView.addObject("message", "Op dit moment kunnen alleen particulier rekeningen worden geopend, klik de link om een rekening te openen.");
         modelAndView.addObject("customerName", user.getFirstName());
-        modelAndView.addObject("bankAccountNameForm", new BankAccountNameForm());
+        BankAccountNameForm bankAccountNameForm = new BankAccountNameForm();
+
+        int aantalRekeningenVanUser = bankAccountDao.findAllByAccountHolder(user).size();
+        bankAccountNameForm.setBankAccountName(user.getFirstName() + "'s rekening " + (aantalRekeningenVanUser+1));
+        modelAndView.addObject("bankAccountNameForm", bankAccountNameForm);
         return modelAndView;
     }
-
-
 }
 
 
