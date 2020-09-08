b990a208e9f8f7f47a67c52b598af820289e3ecf
Manuel Oostveen <oostvem1@ad.hva.nl>
2020-09-08 13:33:14 +0200

    wip

--- src/main/java/nl/dagobank/webapp/controller/HeadBusinessController.java
+++ src/main/java/nl/dagobank/webapp/controller/HeadBusinessController.java
@@ -1,15 +1,11 @@
 package nl.dagobank.webapp.controller;
 
-import nl.dagobank.webapp.domain.BankAccount;
 import nl.dagobank.webapp.domain.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 
 @Controller
 @SessionAttributes( "user")
@@ -18,7 +14,6 @@ public class HeadBusinessController {
     @GetMapping( "overzichtmkb" )
     public String overview( Model model ) {
         if ( model.containsAttribute( "user" )) {
-            System.out.println("Model/session heeft ingelogde user");
             Employee employee = (Employee) model.getAttribute( "user" );
             if ( employee != null && employee.getRole().equals( "HoofdMKB" )) {
                 return "overviewHeadBusiness";
@@ -26,7 +21,6 @@ public class HeadBusinessController {
                 return "geenToegang";
             }
         } else {
-            System.out.println("Model/session heeft geen ingelogde user");
             return "geenToegang";
         }
     }
