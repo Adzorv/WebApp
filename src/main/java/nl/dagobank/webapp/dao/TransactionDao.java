f9ff01ec60ecc94846f46178e16b18a3bf679f08
Vincent van der Struis <vincent.van.der.struis@hva.nl>
2020-09-10 15:59:20 +0200

    transactie overzicht is correct + tests

--- src/main/java/nl/dagobank/webapp/dao/TransactionDao.java
+++ src/main/java/nl/dagobank/webapp/dao/TransactionDao.java
@@ -11,23 +11,10 @@ import java.util.Optional;
 @Repository
 public interface TransactionDao extends CrudRepository<Transaction, Integer> {
 
-    // Alle bij en afschrijvingen per iban
-//    List<Transaction> findAllByIban(Iban iban);
-
-//    List<Transaction> findAllByAccountName(String accountName);
-
-
     List<Transaction> findAllByDebitAccount(BankAccount debitAccount );
     List<Transaction> findAllByCreditAccount(BankAccount creditAccount );
 
-
-//    List<Email> findBy.....(List<String> emails, List<String> pinCodes);
-
-//    List<Email> findByEmailIdInAndPincodeIn(List<String> emails, List<String> pinCodes);
-
-
-
-
+    List<Transaction> findAllByDebitAccountOrCreditAccountOrderByDate(BankAccount debitAccount, BankAccount creditAccount);
 
 }
 
