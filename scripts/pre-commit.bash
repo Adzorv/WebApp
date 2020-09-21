35b50715d508cf18523967ed8bb25b2983aceedd
Manuel Oostveen <oostvem1@ad.hva.nl>
2020-09-21 11:48:07 +0200

    test

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -3,6 +3,9 @@
 echo "Running pre-commit hook"
 ./scripts/run-tests.bash
 
+echo $?
+echo ".........."
+
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
  echo "Tests must pass before commit!"
