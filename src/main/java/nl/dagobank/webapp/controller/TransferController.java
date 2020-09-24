e005133310ec2d26fee76ae268e1243f8c461e98
Manuel Oostveen <oostvem1@ad.hva.nl>
2020-09-24 13:28:45 +0200

    iban naam validatie werkt met ajax

--- src/main/java/nl/dagobank/webapp/controller/TransferController.java
+++ src/main/java/nl/dagobank/webapp/controller/TransferController.java
@@ -1,10 +1,12 @@
 package nl.dagobank.webapp.controller;
 
+import nl.dagobank.webapp.backingbeans.AjaxIbanCheckResponse;
 import nl.dagobank.webapp.backingbeans.TransferForm;
 import nl.dagobank.webapp.domain.BankAccount;
 import nl.dagobank.webapp.service.BankAccountService;
 import nl.dagobank.webapp.service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,20 +30,31 @@ public class TransferController {
     TransferService transferService;
 
 
-    @GetMapping("/checkIban")
-    public ResponseEntity getBankAccount(@RequestParam("iban") String iban,
-                                         @RequestParam("fullname") String fullName) {
+    @GetMapping("checkIban")
+    @ResponseBody
+    public AjaxIbanCheckResponse getBankAccount( String iban, String fullname ) {
+
         BankAccount bankAccount = bankAccountService.findBankAccountByIban(iban);
+        AjaxIbanCheckResponse response = new AjaxIbanCheckResponse();
+        response.setIbanCorrect( false );
         if (bankAccount == null) {
-            return ResponseEntity.badRequest().body("Wrong/Not existing IBAN!");
+            response.setMsg( "Wrong/Not existing IBAN!" );
+            return response;
         }
-        if(!bankAccount.getAccountName().equals(fullName)){
-            return ResponseEntity.badRequest().body("IBAN doesn't match receiver full name!");
+        StringBuilder fullName = new StringBuilder();
+        fullName.append( bankAccount.getAccountHolder().getFullName().getFirstName() )
+                .append( " " )
+                .append( bankAccount.getAccountHolder().getFullName().getLastName() );
+
+        if(!fullName.toString().equals(fullname)){
+            response.setMsg( "IBAN doesn't match receiver full name!" );
+            return response;
         }
-        return ResponseEntity.ok(bankAccount);
+        response.setMsg( "" );
+        return response;
     }
 
-    @GetMapping("/transfer{id}")
+    @GetMapping("/transfer")
     public ModelAndView transactionHandler(@RequestParam("id") int id, Model model) {
         BankAccount selectedBankAccount = bankAccountService.getBankAccountById(id);
         ModelAndView modelAndView = new ModelAndView("transfer");
@@ -50,7 +63,7 @@ public class TransferController {
         return modelAndView;
     }
 
-    @PostMapping("/executeTransfer{id}")
+    @PostMapping("/executeTransfer")
     public String executeTransferHandler(@RequestParam("id") int id,
                                          @ModelAttribute TransferForm transferForm) {
         BankAccount senderBankAccount = bankAccountService.getBankAccountById(id);
@@ -63,7 +76,7 @@ public class TransferController {
         }
     }
 
-    @RequestMapping("/transferError{id}")
+    @RequestMapping("/transferError")
     public String getTransferErrorPage(Model model) {
         model.addAttribute("errorMessage", "ERROR: Insufficient fund to perform this operation!");
         return "transferError";
