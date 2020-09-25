bbc88818019a8337b929e3a5c1dccd7cbd1631bf
Sebastian Holzapfel <sebastian.holzapfel@hva.nl>
2020-09-25 02:21:17 +0200

    deleted old files and disabled nonfunctional datageneration tests

--- src/test/java/nl/dagobank/webapp/service/TestDataUserServiceTest.java
+++ src/test/java/nl/dagobank/webapp/service/TestDataUserServiceTest.java
@@ -1,21 +1,21 @@
 //TODO write real test
-package nl.dagobank.webapp.service;
-
-import org.iban4j.Iban;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class TestDataUserServiceTest {
-
-    @Autowired
-    TestDataUserService service;
-
-    @Test
-    void createAndSaveUsersTest() {
-    service.createAndSaveUsers(5000);
-
-    }
-
-}
+//package nl.dagobank.webapp.service;
+//
+//import org.iban4j.Iban;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class TestDataUserServiceTest {
+//
+//    @Autowired
+//    TestDataUserService service;
+//
+//    @Test
+//    void createAndSaveUsersTest() {
+//    service.createAndSaveUsers(5000);
+//
+//    }
+//
+//}
